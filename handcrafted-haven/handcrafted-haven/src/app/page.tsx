be8105ab1586b7bd8d7a7a6ea09abb4244c9b984
marcosencarnacion <enc24001@byui.edu>
2026-01-28 21:20:57 -0500

    updated file names

--- handcrafted-haven/handcrafted-haven/src/app/page.tsx
+++ handcrafted-haven/handcrafted-haven/src/app/page.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import AboutSection from "@/app/ui/aboutSection";
 import Footer from "@/app/ui/footer";
-import Navbar from '@/app/ui/Navbar';
-import HeroSection from "@/app//ui/HeroSection";
+import Navbar from '@/app/ui/navbar';
+import HeroSection from "@/app/ui/heroSection";
 import CallToAction from "@/app/ui/callToAction";
-import CommunityValues from '@/app/ui/CommunityValues';
+import CommunityValues from '@/app/ui/communityValues';
 
 
 export default function Home() {
