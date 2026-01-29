6bb29b93c3dd2d8e7049f587a180dc8d92e77cac
bruninrko123 <balves@byupathway.edu>
2026-01-28 22:29:02 -0300

    submit week03

--- handcrafted-haven/handcrafted-haven/src/app/page.tsx
+++ handcrafted-haven/handcrafted-haven/src/app/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import AboutSection from './ui/AboutSection';
-import Footer from './ui/Footer';
-import Navbar from './ui/Navbar';
-import HeroSection from './ui/HeroSection';
-import CallToAction from './ui/CallToAction';
-import CommunityValues from './ui/CommunityValues';
+import AboutSection from "@/app/ui/aboutSection";
+import Footer from "@/app/ui/footer";
+import Navbar from '@/app/ui/Navbar';
+import HeroSection from "@/app//ui/HeroSection";
+import CallToAction from "@/app/ui/callToAction";
+import CommunityValues from '@/app/ui/CommunityValues';
 
 
 export default function Home() {
