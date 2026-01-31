c5a94689c3a3d9a95e400df60cdb34ffd6208032
bruninrko123 <balves@byupathway.edu>
2026-01-31 01:33:29 -0300

    fixing a path

--- handcrafted-haven/handcrafted-haven/src/app/page.tsx
+++ handcrafted-haven/handcrafted-haven/src/app/page.tsx
@@ -2,12 +2,10 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import AboutSection from "@/app/ui/aboutSection";
 import Footer from "@/app/ui/footer";
-import Navbar from '@/app/ui/navbar';
-import SampleGalery from '@/app/ui/sampleGalery';
+import Navbar from "@/app/ui/Navbar";
+import SampleGalery from "@/app/ui/sampleGalery";
 import HeroSection from "@/app/ui/heroSection";
 import CallToAction from "@/app/ui/callToAction";
-
-
 
 export default function Home() {
   return (
