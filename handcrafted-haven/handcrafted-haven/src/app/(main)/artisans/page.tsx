23cb86bc2f07985022a0de3df9559c68be6b816c
marcosencarnacion <enc24001@byui.edu>
2026-02-14 10:41:06 -0500

    Enhance Artisans page styling and responsive layout

--- handcrafted-haven/handcrafted-haven/src/app/(main)/artisans/page.tsx
+++ handcrafted-haven/handcrafted-haven/src/app/(main)/artisans/page.tsx
@@ -3,8 +3,6 @@ import ArtisansList from "@/app/ui/artisans/artisansList";
 export default function ArtisansPage() {
   return (
     <>
-      <p>{"This is the artisan's page"}</p>
-
       <ArtisansList />
     </>
   );
