3744f4192bf0c6c76c7a34454abf85ae677bf445
bruninrko123 <balves@byupathway.edu>
2026-02-16 20:44:46 -0300

    awaiting params to fix deployment

--- handcrafted-haven/handcrafted-haven/src/app/api/products/[id]/route.ts
+++ handcrafted-haven/handcrafted-haven/src/app/api/products/[id]/route.ts
@@ -76,7 +76,7 @@ export async function GET(
 
 export async function PUT(
   request: Request,
-  { params }: { params: { id: string } },
+  { params }: { params: Promise<{ id: string }> },
 ) {
   try {
     const data = await request.json();
@@ -114,7 +114,7 @@ export async function PUT(
 
 export async function DELETE(
   request: Request,
-  { params }: { params: { id: string } },
+  { params }: { params: Promise<{ id: string }> },
 ) {
   try {
     await connectToDatabase();
